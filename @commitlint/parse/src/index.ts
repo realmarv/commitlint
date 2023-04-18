6d8cf94527a07adcb01a8d5af0526f9edd33bc85
realmarv <zahratehraninasab@gmail.com>
2023-04-18 17:55:09 +0330

    wip3

--- @commitlint/parse/src/index.ts
+++ @commitlint/parse/src/index.ts
@@ -12,7 +12,7 @@ export default async function parse(
 	const opts = {
 		...defaultOpts,
 		fieldPattern: null,
-		headerPattern: /^(\w*)(?:\((.*)\))?: (.*)|(\w*)(?:\((.*)\))$/,
+		headerPattern: /^(\w*)(?:\((.*)\)(?::)?|:)\s*(.*)$/,
 		...(parserOpts || {}),
 	};
 	const parsed = parser(message, opts) as Commit;
